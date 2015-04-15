21594f656e24c563eedfe552636c714bb8b5d4f1
Yury Rachkouski <6314166@gmail.com>
2015-04-15 07:59:57 +0300

    Task1
    .Net1

---
+++ ConsoleApplication1/LinearFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class LinearFunction
+    {
+        public double A { get; set; }
+
+
+        public double B { get; set; }
+
+        public double Y(double x) { return A * x + B; }
+        public double Y(double a, double b, double x)
+        {
+            A = a;
+            B = b;
+            return Y(x); 
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearFunction linearFunction = new LinearFunction(){A = 10, B = 10};
+            double x = 4.5;
+
+            Console.WriteLine("A={0} B={1}  Y={0}*{2}+{1}={3}", linearFunction.A, linearFunction.B, x, linearFunction.Y(x));
+            Console.ReadKey();
+        }
+    }
+}
